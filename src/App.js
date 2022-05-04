0d7dfa4306519746e6c925617213b8c3a980be2b
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-04 14:45:22 +0100

    added frontend and backend page.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage.js";
 import Careers from "./pages/CareersPage/CareersPage";
 import Contribute from "./pages/ContributePage/ContributePage";
+import Frontend from "./pages/FrontendPage/FrontendPage";
+import Backend from "./pages/BackendPage/BackendPage";
+// import Blockchain from "./pages/BlockchainPage/BlockchainPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/careers" element={<Careers />} />
         <Route path="/contribute" element={<Contribute />} />
+        <Route path="/frontend" element={<Frontend />} />
+        <Route path="/backend" element={<Backend />} />
+        {/* <Route path="/blockchain" element={<Blockchain />} /> */}
         {/* <Route path="/post/:id" element={<Post />} /> */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
---
+++ src/pages/FrontendPage/FrontendPage.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Banner from "../../components/Banner/Banner";
+import ContributeCTA from "../../components/ContributeCTA/ContributeCTA";
+import Footer from "../../components/Footer/Footer";
+
+const FrontendPage = () => {
+  const title = "";
+  const quote =
+    '"Websites promote you 24/7: No employee will do that." – Paul Cookson';
+  const image =
+    "https://i.ibb.co/vYLYxmH/pankaj-patel-Ylk5n-nd9d-A-unsplash.jpg";
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+  return (
+    <div className="contribute-page">
+      <NavBar />
+      <Banner title={title} quote={quote} image={image} />
+      <main></main>
+      <ContributeCTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default FrontendPage;
