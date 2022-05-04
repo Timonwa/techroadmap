095329c8654526ede0009c40b92aacf75219d9a1
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-04 14:27:59 +0100

    changed the variables from css to scss.

--- src/styles/styled-components/ResourceSectionBody.styled.js
+++ src/styles/styled-components/ResourceSectionBody.styled.js
@@ -4,6 +4,6 @@ export const ResourceSectionBody = styled.div`
   list-style-type: disc;
   margin-bottom: 15px;
   a {
-    border-bottom: 1px solid var(--main-color);
+    border-bottom: 1px solid hsl(212, 28%, 86%);
   }
 `;
