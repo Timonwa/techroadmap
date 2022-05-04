873055a145d93a175d3a514c0a46adff75f2f28e
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-04 13:49:01 +0100

    added badge styled components.

--- src/styles/styled-components/Badge.styled.js
+++ src/styles/styled-components/Badge.styled.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
-export const Badge = styled.div`
-  background-color: hsl(0, 0%, 93%);
-  padding: 20px;
-  text-align: center;
+export const Badge = styled.span`
+  border: 1px solid rgba(12, 37, 51, 0.904);
+  padding: 2px 5px;
+  font-size: 50%;
   font-weight: bold;
+  background-color: rgba(21, 68, 95, 0.651);
+  border-radius: 5px;
+  margin-left: 5px;
 `;
