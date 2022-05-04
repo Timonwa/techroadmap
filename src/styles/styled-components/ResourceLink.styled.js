9db17bb0d9a87883be97c268ab6c0af4d0dcf61a
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-04 14:10:55 +0100

    added resource link styled components.

---
+++ src/styles/styled-components/ResourceLink.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ResourceLink = styled.li`
+  list-style-type: disc;
+  margin-bottom: 15px;
+  a {
+    border-bottom: 1px solid var(--main-color);
+  }
+`;
