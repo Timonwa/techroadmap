67c969b81b0ab406086a26d11bb5096781abd904
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-06 17:31:28 +0100

    added backend side bar.

--- src/pages/BackendPage/BackendPage.js
+++ src/pages/BackendPage/BackendPage.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavSpace from "../../components/NavBar/NavSpace";
 import NavBar from "../../components/NavBar/NavBar";
+import SideBar from "../../components/SideBar/SideBar";
+import BackendNav from "../../components/backendPageContent/BackendNav";
 import Banner from "../../components/Banner/Banner";
 import Intro from "../../components/backendPageContent/Intro";
 import WhatIs from "../../components/backendPageContent/WhatIs";
@@ -23,10 +25,19 @@ const BackendPage = () => {
     window.scroll(0, 0);
   }, []);
 
+  const [openMenu, setOpenMenu] = useState(true);
+
+  const handleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <div className="contribute-page">
       <NavSpace />
       <NavBar />
+      <SideBar handleMenu={handleMenu} openMenu={openMenu}>
+        <BackendNav handleMenu={handleMenu} openMenu={openMenu} />
+      </SideBar>
       <Banner title={title} quote={quote} image={image} />
       <main>
         <Intro />
