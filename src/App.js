f518cde4b3e0725af246a66cf0dfa84b31a2653d
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-08 17:12:29 +0100

    replaced google analyitics depencency with natural google analytics.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,7 @@ import Backend from "./pages/BackendPage/BackendPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 // import Blockchain from "./pages/BlockchainPage/BlockchainPage";
 
-import ReactGA from "react-ga4";
-
 function App() {
-  const TRACKING_ID = "UA-228201739-1"; // OUR_TRACKING_ID
-  ReactGA.initialize(TRACKING_ID);
-
-  useEffect(() => {
-    ReactGA.send("pageview");
-  }, []);
-
   return (
     <div className="App">
       <Routes>
