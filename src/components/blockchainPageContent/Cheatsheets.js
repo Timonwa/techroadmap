d7da71c69d4aceda5c5c8c8d00a702a08e2d2f11
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-14 10:16:09 +0100

    added blockchain cheatsheet section.

--- src/components/blockchainPageContent/Cheatsheets.js
+++ src/components/blockchainPageContent/Cheatsheets.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { ResourceCardLinks } from "../../styles/styled-components/ResourceCardLinks.styled";
+import { ResourceLink } from "../../styles/styled-components/ResourceLink.styled";
+import { ResourceSection } from "../../styles/styled-components/ResourceSection.styled";
+import { ResourceSectionBody } from "../../styles/styled-components/ResourceSectionBody.styled";
+import { ResourceSectionHeading } from "../../styles/styled-components/ResourceSectionHeading.styled";
 
 const Cheatsheets = () => {
-  return <div>Cheatsheets</div>;
+  return (
+    <ResourceSection id="cheatsheets">
+      <ResourceSectionHeading>Cheatsheets</ResourceSectionHeading>
+
+      <ResourceSectionBody>
+        <p>
+          Cheatsheets are short lists of information that you can use to help
+          you do or remember something. Below are cheatsheets on what you have
+          been learning so far. Do enjoy!
+        </p>
+      </ResourceSectionBody>
+
+      <ResourceCardLinks>
+        <ResourceLink>
+          <a href="#" target="_blank"></a>
+        </ResourceLink>
+      </ResourceCardLinks>
+    </ResourceSection>
+  );
 };
 
 export default Cheatsheets;
