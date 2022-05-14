c1a5d5d22d7a7dfad07429d6a02258a81708b083
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-14 10:06:06 +0100

    added blockchain intro section.

--- src/components/blockchainPageContent/Intro.js
+++ src/components/blockchainPageContent/Intro.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Intro() {
-  return <div>Intro</div>;
+  return (
+    <section className="intro">
+      <div className="intro-wrapper">
+        <h2 className="heading">Begin Your Blockchain Developer Journey!</h2>
+        <div className="intro__body">
+          <p>
+            Congratulations on choosing this career path. You are one step
+            closer to becoming a blockchain developer. This guide is specially to
+            help you have a smooth learning, with easy access to the right
+            resources and data.
+          </p>
+          <p>
+            You will find here vast resources on blockchain development from who
+            exactly is blockchain developer, the blockchain development roadmap,
+            blockchain career paths, blockchain development skills, tools to use,
+            projects to build, communities to help you grow and network, videos,
+            articles, etc.
+          </p>
+          <p>Congrats once again.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Intro;
