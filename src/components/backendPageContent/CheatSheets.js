e5a042f73aebee2a10a347f0939202381141fc2a
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-16 20:15:42 +0100

    Added download to link

--- src/components/backendPageContent/CheatSheets.js
+++ src/components/backendPageContent/CheatSheets.js
@@ -21,7 +21,7 @@ const CheatSheets = () => {
 
       <ResourceCardLinks>
         <ResourceLink>
-          <a href="../../assets/backend/cheatsheets/test.txt" >Test file</a>
+          <a href="../../assets/backend/cheatsheets/test.txt" download>Test file</a>
         </ResourceLink>
       </ResourceCardLinks>
     </ResourceSection>
