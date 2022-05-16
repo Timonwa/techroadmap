f6eefa298227aff3b6d5079f1c953636705b4f27
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-16 10:03:57 +0100

    installed react snap but currently not using it
    it is not compatible with current version of react.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { hydrate, render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "../src/styles/css-resets/normalize.css";
 import "../src/styles/css-resets/my-css-reset.css";
@@ -15,3 +16,10 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
