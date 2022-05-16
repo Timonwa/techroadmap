99327fef29329294ac1b07f0dd723415b29d835e
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-16 20:06:52 +0100

    Added download attribute to cheatsheet link

--- src/components/backendPageContent/CheatSheets.js
+++ src/components/backendPageContent/CheatSheets.js
@@ -21,7 +21,7 @@ const CheatSheets = () => {
 
       <ResourceCardLinks>
         <ResourceLink>
-          <Link to="../../assets/backend/cheatsheets/test.txt">Test file</Link>
+          <Link to="../../assets/backend/cheatsheets/test.txt" download>Test file</Link>
         </ResourceLink>
       </ResourceCardLinks>
     </ResourceSection>
