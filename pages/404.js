bcc72ae29c0d9c2f936b450c80e4f64db64e7e7e
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-21 15:16:44 +0100

    added 404 page.

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NotFound from "../components/NotFound";
+
+const NotFoundPage = () => {
+  return (
+    <div className="notFoundPage">
+      <main>
+        <NotFound />
+      </main>
+    </div>
+  );
+};
+
+export default NotFoundPage;
