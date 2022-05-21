8a834ad505cf280103df0cf21e8d6d7c96c839c7
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-05-21 12:32:42 +0100

    dded google analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,28 @@ import ContributeCTA from "../components/ContributeCTA";
 import Footer from "../components/Footer";
 import NavSpace from "../components/NavSpace";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <NavBar />
